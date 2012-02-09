662e617d9cadc31ef2a4feb5cda27685141831ef
Jon Gjengset <jon@thesquareplanet.com>
2012-02-09 11:19:18 +0100

    Fixed gsc and added 301, 302, 303

--- .vim/snippets/php/gsc.sh
+++ .vim/snippets/php/gsc.sh
@@ -4,12 +4,12 @@
 # Fetch list from Wikipedia
 curl -s http://en.wikipedia.org/wiki/List_of_HTTP_status_codes | \
 # Find status codes
-grep dt | grep -P '</span>\d{3}' | \
+grep dt | grep -P 'span id="\d{3}"' | \
 # Extract status code number and text
-perl -pe "s/^.*span>(\d{3})\s([\w\s\-']+?)\s*(\(|<).*/\1 \2/" | \
+perl -pe "s/^.*span>(<a[^>]+>)?(\d{3})\s([\w\s\-']+?)\s*(\(|<).*/\2 \3/" | \
 # Escape quotes (I'm a teapot)
 sed "s/'/\\\\\'/" | \
 # Pass through xargs
 # Use this line if you want to include 418 (this puts all header strings in double quotes to cater for the ' in I'm)
 #xargs -L 1 bash -c "echo 'header(\"HTTP/1.1' \"\$0\" \"\$*\"'\");' > \$0.snippet; echo \"Found \$0 \$*\""
-grep -v 418 | xargs -L 1 bash -c "echo \"header('HTTP/1.1\" \"\$0\" \"\$*');\" > \$0.snippet; echo \"Found \$0 \$*\""
+grep -v 418 | xargs -L 1 bash -c "echo \"header('HTTP/1.1\" \"\$0\" \"\$*');\" > \$0.snippet; echo \"Found \$0 \$*\"";
