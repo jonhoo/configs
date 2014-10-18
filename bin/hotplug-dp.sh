47f6f18e96d9823f8a60b3af7fe4bf8207f44535
Jon Gjengset <jon@thesquareplanet.com>
2014-10-18 15:58:11 -0400

    Better screen management (e.g. res)

--- bin/hotplug-dp.sh
+++ bin/hotplug-dp.sh
@@ -5,48 +5,57 @@ read STATUS_HDMI < /sys/class/drm/card0-HDMI-A-1/status
 export DISPLAY=:0
 export XAUTHORITY=/home/jon/.Xauthority
 
+maxlight() {
+	cat /sys/class/backlight/intel_backlight/max_brightness > /sys/class/backlight/intel_backlight/brightness
+}
+
 DEV=""
+DEVC=""
 STATUS="disconnected"
 if [[ "$STATUS_DP" = "connected" ]]; then
 	STATUS="conected"
 	DEV="DP1"
+	DEVC="DP-1"
 elif [[ "$STATUS_HDMI" = "connected" ]]; then
 	STATUS="connected"
 	DEV="HDMI1"
+	DEVC="HDMI-A-1"
 fi
 
 if [ "$STATUS" = "disconnected" ]; then
-  xrandr --output DP1 --off
-  xrandr --output HDMI1 --off
-  xrandr --output LVDS1 --mode 1600x900
-  xset +dpms
-  xset s default
-  if $(pgrep tint2 > /dev/null); then
-	  pkill tint2; sudo -u jon tint2 &
-  elif $(pgrep trayer > /dev/null); then
-	  pkill trayer; sudo -u jon ~jon/bin/usr-trayer &
-  fi
-  sudo -u jon nitrogen --restore
-  sed -i 's/^/#/' ~jon/.asoundrc
-  sed -i 's/^##/#/' ~jon/.asoundrc
-  sed -i 's/HandleLidSwitch\=ignore/HandleLidSwitch\=suspend/' /etc/systemd/logind.conf
+	xrandr --output DP1 --off
+	xrandr --output HDMI1 --off
+	xrandr --output LVDS1 --mode 1600x900
+	xset +dpms
+	xset s default
+	sed -i 's/^/#/' ~jon/.asoundrc
+	sed -i 's/^##/#/' ~jon/.asoundrc
+	sed -i 's/HandleLidSwitch\=ignore/HandleLidSwitch\=suspend/' /etc/systemd/logind.conf
 else
-  if [[ $1 == "mirror" ]]; then
-    xrandr --output $DEV --mode 1024x768
-    xrandr --output LVDS1 --mode 1024x768 --same-as $DEV
-  else
-    xrandr --output $DEV --above LVDS1 --mode 1920x1080
-  fi
-  xset -dpms
-  xset s off
-  if $(pgrep tint2 > /dev/null); then
-	  pkill tint2; sudo -u jon tint2 &
-  elif $(pgrep trayer > /dev/null); then
-	  pkill trayer; sudo -u jon ~jon/bin/usr-trayer &
-  fi
-  sudo -u jon nitrogen --restore
-  sed -i 's/^#//' ~jon/.asoundrc
-  sed -i 's/HandleLidSwitch\=suspend/HandleLidSwitch\=ignore/' /etc/systemd/logind.conf
+	if [[ $1 == "mirror" ]]; then
+		xrandr --output $DEV --mode 1024x768
+		xrandr --output LVDS1 --mode 1024x768 --same-as $DEV
+	else
+		edid=$(cat /sys/class/drm/card0/card0-$DEVC/edid | sha512sum - | sed 's/\s*-$//')
+
+		res=$(xrandr -q | grep -A1 "$DEV connected" | tail -n 1 | sed 's/^ *\([0-9x]*\).*/\1/')
+		xrandr --output $DEV --above LVDS1 --mode $res
+	fi
+	xset -dpms
+	xset s off
+	sed -i 's/^#//' ~jon/.asoundrc
+	sed -i 's/HandleLidSwitch\=suspend/HandleLidSwitch\=ignore/' /etc/systemd/logind.conf
 fi
+
+if $(pgrep tint2 > /dev/null); then
+	pkill tint2; sudo -u jon tint2 &
+fi
+if $(pgrep trayer > /dev/null); then
+	pkill trayer; sudo -u jon ~jon/bin/usr-trayer &
+fi
+if $(pgrep xmobar > /dev/null); then
+	sudo -u jon pkill -USR1 xmobar
+fi
+sudo -u jon nitrogen --restore
 
 systemctl restart systemd-logind
