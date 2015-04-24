a685c92598b40bec4414a6952a4018fa7094f087
Jon Gjengset <jon@thesquareplanet.com>
2015-04-24 15:22:24 -0400

    Update taffy config for new version

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -5,29 +5,29 @@ import System.Taffybar.TaffyPager
 import System.Taffybar.Pager
 import System.Taffybar.SimpleClock
 import System.Taffybar.FreedesktopNotifications
-import System.Taffybar.MPRIS
 import Graphics.UI.Gtk
 
 textWidgetNew :: String -> IO Widget
 textWidgetNew str = do
-	box <- hBoxNew False 0
-	label <- labelNew $ Just str
-	boxPackStart box label PackNatural 0
-	widgetShowAll box
-	return $ toWidget box
+    box <- hBoxNew False 0
+    label <- labelNew $ Just str
+    boxPackStart box label PackNatural 0
+    widgetShowAll box
+    return $ toWidget box
 
 main = do
-  let clock = textClockNew Nothing "%-I:%M %p, %A %B %d" 1
-      pager = taffyPagerNew defaultPagerConfig { widgetSep      = " :: "
-					       , emptyWorkspace = colorize "gray" "" . escape
-					       }
-      mpris = mprisNew
-      tray = systrayNew
-      sep = textWidgetNew " ::"
-  defaultTaffybar defaultTaffybarConfig { startWidgets = [ clock, sep, pager ]
-                                        , endWidgets = [ mpris, tray ]
-					, barHeight = 20
-					, monitorNumber = 1
-					, widgetSpacing = 0
-					, barPosition = Bottom
-                                        }
+    let clock = textClockNew Nothing "%-I:%M %p, %A %B %d" 1
+        pager = taffyPagerNew defaultPagerConfig
+            { widgetSep = " :: "
+            , emptyWorkspace = colorize "gray" "" . escape
+            }
+        tray = systrayNew
+        sep = textWidgetNew " ::"
+    defaultTaffybar defaultTaffybarConfig
+                        { startWidgets = [ clock, sep, pager ]
+                        , endWidgets = [ tray ]
+                        , barHeight = 20
+                        , monitorNumber = 1
+                        , widgetSpacing = 0
+                        , barPosition = Bottom
+                        }
