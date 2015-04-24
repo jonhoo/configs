ae612ecabd7bb4d0c743abf1953f4cab621cb4cb
Jon Gjengset <jon@thesquareplanet.com>
2015-04-24 15:21:57 -0400

    Better fish greeting

--- fish/config.fish
+++ fish/config.fish
@@ -108,9 +108,22 @@ eval sh $HOME/dev/others/base16/shell/base16-atelierdune.dark.sh
 
 function fish_greeting
 	tput cup $LINES # start terminal at the bottom
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e " \\e[1mDisk usage:\\e[0m"
+	echo
+	echo -ne (\
+		df -h | grep -E 'sda|mapper' | \
+		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
+		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
+		paste -sd ''\
+	)
+
+	echo
+	echo -e " \\e[1mAnd here's a quote:\\e[0m"
 	set_color blue
-	echo '\\'
-	fortune -asn 500 $FORTUNES | sed 's/^/ > /'
-	echo '/'
+	fortune -asn 500 $FORTUNES | sed 's/^/ /'
+	echo
 	set_color normal
 end
