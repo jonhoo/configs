ab2861848c908b138a2c85f1213808077464b1ef
Jon Gjengset <jon@thesquareplanet.com>
2015-05-26 14:38:25 -0400

    Clean up non-vim files

--- taffybar/taffybar.hs
+++ taffybar/taffybar.hs
@@ -26,7 +26,7 @@ main = do
     defaultTaffybar defaultTaffybarConfig
                         { startWidgets = [ clock, sep, pager ]
                         , endWidgets = [ tray ]
-                        , barHeight = 30
+                        , barHeight = 20
                         , monitorNumber = 1
                         , widgetSpacing = 0
                         , barPosition = Bottom
