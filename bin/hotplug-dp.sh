74815d88d4327614861b56b6914b5f20895d72dc
Jon Gjengset <jon@thesquareplanet.com>
2017-12-21 12:22:39 -0500

    Better edid matching

--- bin/hotplug-dp.sh
+++ bin/hotplug-dp.sh
@@ -61,14 +61,17 @@ else
 		edid=$(/usr/bin/cat /sys/class/drm/card0/card0-$DEVC/edid | /usr/bin/sha512sum - | /usr/bin/sed 's/\s*-$//')
 
 		pos="above"
-		if [[ $edid == "9ed75b31c6f1bce5db7420887ebbc71c126d6a152ddf00b2b5bbb7a5479cea2608273bfcae23d8ec7bcf01578256d672c5fb0d899005f46096ef98dc447d2244" ]]; then
-			pos="primary --rotate left --right-of"
-			maxlight
-		fi
-		if [[ $edid == "e0b1346dd753a490b1d067e6b91acb40b52f67572799f6b7629484a167879248a7550496f85f62a91ea8c3cd96ddbb8cb6e30cba2effa645f225cd22667a278a" ]]; then
-			pos="primary --right-of"
-			maxlight
-		fi
+		case "$edid" in
+			"9ed75b31c6f1bce5db7420887ebbc71c126d6a152ddf00b2b5bbb7a5479cea2608273bfcae23d8ec7bcf01578256d672c5fb0d899005f46096ef98dc447d2244")
+				pos="primary --rotate left --right-of"
+				maxlight
+				;;
+			"2639dca257f1da0fecd2edf771aefae0347b56f2f2e51fb44802859d4b43e6b298b9d157bcb7a179de054435cf97d5af674244d6139319a36a1f58791d392a7e"|\
+			"e0b1346dd753a490b1d067e6b91acb40b52f67572799f6b7629484a167879248a7550496f85f62a91ea8c3cd96ddbb8cb6e30cba2effa645f225cd22667a278a")
+				pos="primary --right-of"
+				maxlight
+				;;
+		esac
 		/usr/bin/xrandr --addmode eDP-1 1920x1080
 		/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --output $DEV --$pos eDP-1 --auto
 	fi
