5dc94421f12495a40840408ba492ff10e46d83c5
Jon Gjengset <jon@thesquareplanet.com>
2018-02-26 22:01:06 -0500

    Add aws function

--- fish/config.fish
+++ fish/config.fish
@@ -185,6 +185,9 @@ setenv OS_TENANT_NAME usersandbox_jfrg
 setenv OS_AUTH_URL https://nimbus.csail.mit.edu:5001/v2.0
 setenv OS_IMAGE_API_VERSION 1
 setenv OS_VOLUME_API_VERSION 2
+function aws -d "Set up environment for AWS"
+	env AWS_SECRET_ACCESS_KEY=(pass www/aws-secret-key | head -n1) $argv
+end
 function penv -d "Set up environment for the PDOS openstack service"
 	env OS_PASSWORD=(pass www/mit-openstack | head -n1) OS_TENANT_NAME=pdos OS_PROJECT_NAME=pdos $argv
 end
