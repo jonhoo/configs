7e721d6f5ed0b0ae1440d4817c28ce7905b0dae6
Jon Gjengset <jon@thesquareplanet.com>
2019-02-05 15:10:18 -0500

    Brand new (denser) prompt!

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,21 +1,20 @@
 function fish_prompt
-  set_color white
+  set_color brblack
   echo -n "["(date "+%H:%M")"] "
-  set_color yellow
-  echo -n (whoami)
-  set_color normal
-  echo -n '@'
+  #set_color yellow
+  #echo -n (whoami)
+  #set_color normal
   set_color blue
-  echo -n (hostname)" "
+  echo -n (hostname)
+  if [ $PWD != $HOME ]
+    set_color brblack
+    echo -n ':'
+    set_color yellow
+    echo -n (basename $PWD)
+  end
   set_color green
-  echo -n (prompt_pwd)
-  set_color brown
   printf '%s ' (__fish_git_prompt)
   set_color red
-  if [ (whoami) = "root" ]
-    echo -n '# '
-  else
-    echo -n '$ '
-  end
+  echo -n '| '
   set_color normal
 end
