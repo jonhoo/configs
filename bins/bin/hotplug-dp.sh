16ad47fc95746fb5c8275b7116f762460361c931
Jon Gjengset <jon@thesquareplanet.com>
2019-02-20 15:06:02 -0500

    More screens!

--- bins/bin/hotplug-dp.sh
+++ bins/bin/hotplug-dp.sh
@@ -3,6 +3,7 @@
 read STATUS_DP < /sys/class/drm/card0-DP-1/status
 read STATUS_DP2 < /sys/class/drm/card0-DP-2/status
 read STATUS_DP3 < /sys/class/drm/card0-DP-3/status
+read STATUS_DP4 < /sys/class/drm/card0-DP-4/status
 read STATUS_HDMI < /sys/class/drm/card0-HDMI-A-1/status
 read STATUS_HDMI2 < /sys/class/drm/card0-HDMI-A-2/status
 export DISPLAY=:0
@@ -10,18 +11,6 @@ export XAUTHORITY=/home/jon/.Xauthority
 
 maxlight() {
 	/bin/cat /sys/class/backlight/intel_backlight/max_brightness > /sys/class/backlight/intel_backlight/brightness
-}
-
-lowdpi() {
-	#/usr/bin/sed -i 's/Xft.dpi: .*/Xft.dpi: 96/' ~jon/.Xresources
-	#/usr/bin/sudo -E -u jon xrdb ~jon/.Xresources
-	:
-}
-
-hidpi() {
-	#/usr/bin/sed -i 's/Xft.dpi: .*/Xft.dpi: 144/' ~jon/.Xresources
-	#/usr/bin/sudo -u jon xrdb ~jon/.Xresources
-	:
 }
 
 DEV=""
@@ -39,6 +28,10 @@ elif [[ "$STATUS_DP3" = "connected" ]]; then
 	STATUS="conected"
 	DEV="DP-1-1"
 	DEVC="DP-3"
+elif [[ "$STATUS_DP4" = "connected" ]]; then
+	STATUS="conected"
+	DEV="DP-1-2"
+	DEVC="DP-4"
 elif [[ "$STATUS_HDMI" = "connected" ]]; then
 	STATUS="connected"
 	DEV="HDMI-1"
@@ -49,6 +42,7 @@ elif [[ "$STATUS_HDMI2" = "connected" ]]; then
 	DEVC="HDMI-A-2"
 fi
 
+dpi=96
 if [ "$STATUS" = "disconnected" ]; then
 	/usr/bin/xrandr --output DP-1 --off
 	/usr/bin/xrandr --output DP-2 --off
@@ -58,7 +52,7 @@ if [ "$STATUS" = "disconnected" ]; then
 	/usr/bin/xrandr --output eDP-1 --auto
 	/usr/bin/xset +dpms
 	/usr/bin/xset s default
-	hidpi
+	dpi=144
 else
 	if [[ $1 == "mirror" ]]; then
 		/usr/bin/xrandr --output $DEV --mode 1024x768
@@ -68,6 +62,7 @@ else
 
 		background_primary="wide"
 		background_secondary="wide"
+		laptop_res="1920x1080"
 		pos="above --rotate normal"
 		case "$edid" in
 			"81ec22b7be37e347de73e6e9c2b8f68d5e34068e3b1f058a7f80a0b37af576198767ffbc0bb3691c4957d863fd224e67d72e6835caf88d418ba2da1f4e0a8b0d")
@@ -75,22 +70,28 @@ else
 				background_primary="tall"
 				maxlight
 				;;
+			"22bddaae13e6f792f1d720f7571e79340455bccacba7aab64b267cf51b1f48d31f49a3894396419a146820279be7221997d5c9bb903b1d61365666c6dd57b2e3")
+				pos="primary --right-of"
+				background_primary="uhd"
+				laptop_res="2560x1440"
+				maxlight
+				dpi=140
+				;;
 			"2639dca257f1da0fecd2edf771aefae0347b56f2f2e51fb44802859d4b43e6b298b9d157bcb7a179de054435cf97d5af674244d6139319a36a1f58791d392a7e"|\
 			"e0b1346dd753a490b1d067e6b91acb40b52f67572799f6b7629484a167879248a7550496f85f62a91ea8c3cd96ddbb8cb6e30cba2effa645f225cd22667a278a")
 				pos="primary --rotate normal --right-of"
 				maxlight
 				;;
 		esac
-		/usr/bin/xrandr --output eDP-1 --mode 1920x1080 --output $DEV --$pos eDP-1 --auto
+		/usr/bin/xrandr --output eDP-1 --mode "$laptop_res" --output $DEV --$pos eDP-1 --auto
 		sleep 1
 		nitrogen --head=0 --set-zoom ~jon/.backgrounds/"$background_primary".jpg
 		nitrogen --head=1 --set-zoom ~jon/.backgrounds/"$background_secondary".jpg
 	fi
 	/usr/bin/xset -dpms
 	/usr/bin/xset s off
-
-	/usr/bin/xrandr --dpi 96
-	lowdpi
 fi
+/usr/bin/xrandr --dpi "$dpi"
+/usr/bin/xrdb -merge <(echo "Xft.dpi: $dpi")
 
 /usr/bin/sudo -E -u jon env HOME=~jon ~jon/.config/polybar/launch.sh
