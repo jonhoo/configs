6cc8a850389ef9565a8aad94b760d97dc7d874d0
Jon Gjengset <jon@thesquareplanet.com>
2021-06-06 14:33:06 -0700

    Better tag-from-carg-toml

--- bins/bin/tag-from-cargo-toml.sh
+++ bins/bin/tag-from-cargo-toml.sh
@@ -36,34 +36,69 @@ function tag() {
 
 # in case we're in a subdir:
 cargo_toml=$(git ls-files --full-name Cargo.toml)
+
 was=""
 last_hash=""
-for hash in $(git log --first-parent --format=oneline Cargo.toml | awk '{print $1}'); do
-	v=$(git show "$hash:$cargo_toml" | grep -P '^version\s*=' | head -n1 | sed -e 's/[^"]*"//' -e 's/".*//')
-	if [[ $was == $v ]]; then
-		# same version -- keep walking until we hit the previous
-		last_hash=$hash
-		continue
-	fi
-	if [[ $was = ${v}-* ]]; then
-		# pushed an alpha/beta/rc *after* main version?
-		# no, main version cannot have been released as non-alpha first
-		continue
-	fi
-	if [[ -z "$was" ]]; then
-		# start search
-		was="$v"
-		last_hash="$hash"
-		continue
-	fi
+last_name=""
+function walk() {
+	local cargo_toml=$1
+	local start_hash=$2
+	for hash in $(git log --first-parent --format=oneline "$start_hash" -- "$cargo_toml" | awk '{print $1}'); do
+		n=$(git show "$hash:$cargo_toml" | grep -P '^name\s*=' | head -n1 | sed -e 's/[^"]*"//' -e 's/".*//')
+		v=$(git show "$hash:$cargo_toml" | grep -P '^version\s*=' | head -n1 | sed -e 's/[^"]*"//' -e 's/".*//')
+		if [[ -z "$last_name" ]]; then
+			last_name=$n
+		elif [[ -z "$n" ]]; then
+			# probably a workspace -- try a little harder to retrace versions
+			local cargo_toml="${cargo_toml%Cargo.toml}$last_name/Cargo.toml"
+			if ! git cat-file -e $hash:$cargo_toml 2>/dev/null; then
+				echo "!! crate disappeared -- stopping." > /dev/stderr
+				return
+			fi
 
-	# this hash has a different hash than the previous one. so tag the last
-	# hash with the last version number, since that was the first hash at
-	# which that version number appeared.
-	tag "$was" "$last_hash"
-	last_hash="$hash"
-	was=$v
-done
+			n=$(git show "$hash:$cargo_toml" | grep -P '^name\s*=' | head -n1 | sed -e 's/[^"]*"//' -e 's/".*//')
+			if [[ -z "$n" ]]; then
+				echo "!! could not determine name of crate in workspace -- stopping." > /dev/stderr
+				return
+			elif [[ $last_name != $n ]]; then
+				echo "!! name changed from $n (in workspace) to $last_name -- stopping." > /dev/stderr
+				return
+			else
+				# restart within workspace
+				echo ".. following crate into workspace $last_name"
+				walk "$cargo_toml" "$hash"
+				return
+			fi
+		elif [[ $last_name != $n ]]; then
+			echo "!! name changed from $n to $last_name -- stopping." > /dev/stderr
+			break
+		fi
+		if [[ $was == $v ]]; then
+			# same version -- keep walking until we hit the previous
+			last_hash=$hash
+			continue
+		fi
+		if [[ $was = ${v}-* ]]; then
+			# pushed an alpha/beta/rc *after* main version?
+			# no, main version cannot have been released as non-alpha first
+			continue
+		fi
+		if [[ -z "$was" ]]; then
+			# start search
+			was="$v"
+			last_hash="$hash"
+			continue
+		fi
+
+		# this hash has a different hash than the previous one. so tag the last
+		# hash with the last version number, since that was the first hash at
+		# which that version number appeared.
+		tag "$was" "$last_hash"
+		last_hash="$hash"
+		was=$v
+	done
+}
+walk "$cargo_toml" "HEAD"
 
 # tag the first commit that has this Cargo.toml
 tag "$was" "$last_hash"
