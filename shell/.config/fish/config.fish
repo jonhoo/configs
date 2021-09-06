fc4be864ce9a3303017c17cf6c2dbdec521cec03
Jon Gjengset <jon@thesquareplanet.com>
2021-09-06 10:59:54 -0700

    Better tmux launch

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -22,7 +22,9 @@ if status --is-interactive
 		set fish_function_path $fish_function_path ~/dev/others/base16/templates/fish-shell/functions
 		builtin source ~/dev/others/base16/templates/fish-shell/conf.d/base16.fish
 	end
-	tmux 2> /dev/null; and exec true
+	if ! set -q TMUX
+		exec tmux
+	end
 end
 
 if command -v aurman > /dev/null
