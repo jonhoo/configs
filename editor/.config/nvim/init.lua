274c48dcb4e6569e824095f241e554f36535db8e
Jon Gjengset <jon@thesquareplanet.com>
2024-01-31 09:16:46 +0100

    Actually start vim-rooter

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -322,7 +322,12 @@ require("lazy").setup({
 	},
 	-- auto-cd to root of git project
 	-- 'airblade/vim-rooter'
-	'notjedi/nvim-rooter.lua',
+	{
+		'notjedi/nvim-rooter.lua',
+		config = function()
+			require('nvim-rooter').setup()
+		end
+	},
 	-- fzf support for ^p
 	{
 		'junegunn/fzf.vim',
