8ec5767a1c92a58e73f7105447c8fc741b31246e
Jon Gjengset <jon@thesquareplanet.com>
2024-01-31 09:21:12 +0100

    Bash LSP!

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -386,6 +386,26 @@ require("lazy").setup({
 				},
 			}
 
+			-- Bash LSP
+			local configs = require 'lspconfig.configs'
+			if not configs.bash_lsp and vim.fn.executable('bash-language-server') == 1 then
+				configs.bash_lsp = {
+					default_config = {
+						cmd = { 'bash-language-server', 'start' },
+						filetypes = { 'sh' },
+						root_dir = require('lspconfig').util.find_git_ancestor,
+						init_options = {
+							settings = {
+								args = {}
+							}
+						}
+					}
+				}
+			end
+			if configs.bash_lsp then
+				lspconfig.bash_lsp.setup {}
+			end
+
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 			vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
