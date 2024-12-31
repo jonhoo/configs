b2b952cbc614363c741e1b18571b1f61c83f1759
Jon Gjengset <jon@thesquareplanet.com>
2024-12-31 09:49:07 +0100

    Adapt to fd no longer ignoring .git/

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -347,13 +347,13 @@ require("lazy").setup({
 				if base == '.' then
 					-- if there is no current file,
 					-- proximity-sort can't do its thing
-					return 'fd --type file --follow'
+					return 'fd --hidden --type file --follow'
 				else
-					return vim.fn.printf('fd --type file --follow | proximity-sort %s', vim.fn.shellescape(vim.fn.expand('%')))
+					return vim.fn.printf('fd --hidden --type file --follow | proximity-sort %s', vim.fn.shellescape(vim.fn.expand('%')))
 				end
 			end
 			vim.api.nvim_create_user_command('Files', function(arg)
-				vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--tiebreak=index' }, arg.bang)
+				vim.fn['fzf#vim#files'](arg.qargs, { source = list_cmd(), options = '--scheme=path --tiebreak=index' }, arg.bang)
 			end, { bang = true, nargs = '?', complete = "dir" })
 		end
 	},
