27b761feeb712a3b6eb3a3613139c4ba984a5e4e
Jon Gjengset <jon@thesquareplanet.com>
2025-08-05 17:46:46 +0200

    Be more forgiving on hosts with other cmds

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -205,7 +205,7 @@ function fish_prompt
 	set_color brblack
 	echo -n "["(date "+%H:%M")"] "
 	set_color blue
-	echo -n (hostnamectl hostname)
+	echo -n (command -q hostname; and hostname; or hostnamectl hostname)
 	if [ $PWD != $HOME ]
 		set_color brblack
 		echo -n ':'
@@ -234,36 +234,38 @@ function fish_greeting
 	)
 	echo
 
-	echo -e " \\e[1mNetwork:\\e[0m"
-	echo
-	# http://tdt.rocks/linux_network_interface_naming.html
-	echo -ne (\
-		ip addr show up scope global | \
-			grep -E ': <|inet' | \
-			sed \
-				-e 's/^[[:digit:]]\+: //' \
-				-e 's/: <.*//' \
-				-e 's/.*inet[[:digit:]]* //' \
-				-e 's/\/.*//'| \
-			awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
-			sort | \
-			column -t -R1 | \
-			# public addresses are underlined for visibility \
-			sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
-			# private addresses are not \
-			sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
-			# unknown interfaces are cyan \
-			sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
-			# ethernet interfaces are normal \
-			sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
-			# wireless interfaces are purple \
-			sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
-			# wwan interfaces are yellow \
-			sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
-			sed 's/$/\\\e[0m/' | \
-			sed 's/^/\t/' \
-		)
-	echo
+	if command -q ip
+		echo -e " \\e[1mNetwork:\\e[0m"
+		echo
+		# http://tdt.rocks/linux_network_interface_naming.html
+		echo -ne (\
+			ip addr show up scope global | \
+				grep -E ': <|inet' | \
+				sed \
+					-e 's/^[[:digit:]]\+: //' \
+					-e 's/: <.*//' \
+					-e 's/.*inet[[:digit:]]* //' \
+					-e 's/\/.*//'| \
+				awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
+				sort | \
+				column -t -R1 | \
+				# public addresses are underlined for visibility \
+				sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
+				# private addresses are not \
+				sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
+				# unknown interfaces are cyan \
+				sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
+				# ethernet interfaces are normal \
+				sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
+				# wireless interfaces are purple \
+				sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
+				# wwan interfaces are yellow \
+				sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
+				sed 's/$/\\\e[0m/' | \
+				sed 's/^/\t/' \
+			)
+		echo
+	end
 
 	set r (random 0 100)
 	if [ $r -lt 5 ] # only occasionally show backlog (5%)
