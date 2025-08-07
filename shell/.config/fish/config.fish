a711a6483dbab74a2892ee02e243a0a4abe99333
Jon Gjengset <jon@thesquareplanet.com>
2025-08-07 14:31:29 +0200

    nvme drives are drives too

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -227,7 +227,7 @@ function fish_greeting
 	echo -e " \\e[1mDisk usage:\\e[0m"
 	echo
 	echo -ne (\
-		df -l -h | grep -E 'dev/(xvda|sd|mapper)' | \
+		df -l -h | grep -E 'dev/(xvda|sd|mapper|nvme)' | \
 		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
 		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
 		paste -sd ''\
