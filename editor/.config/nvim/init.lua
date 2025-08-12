bdbbf6932ee2fb3d64044b62c19ca1793c13399b
Jon Gjengset <jon@thesquareplanet.com>
2025-08-12 16:46:48 +0200

    improved NoNeckPain

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -333,14 +333,26 @@ require("lazy").setup({
 		opts = {
 			mappings = {
 				enabled = true,
-				toggle = '<leader>t',
+				toggle = false,
 				toggleLeftSide = false,
 				toggleRightSide = false,
 				widthUp = false,
 				widthDown = false,
 				scratchPad = false,
 			}
-		}
+		},
+		config = function()
+			vim.keymap.set('', '<leader>t', function()
+				vim.cmd([[
+					:NoNeckPain
+					:set formatoptions-=tc linebreak tw=0 cc=0 wrap wm=20 noautoindent nocindent nosmartindent indentkeys=
+				]])
+				-- make 0, ^ and $ behave better in wrapped text
+				vim.keymap.set('n', '0', 'g0')
+				vim.keymap.set('n', '$', 'g$')
+				vim.keymap.set('n', '^', 'g^')
+			end)
+		end
 	},
 	-- auto-cd to root of git project
 	-- 'airblade/vim-rooter'
