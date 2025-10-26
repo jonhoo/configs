c54f2c7231fd5e61ef10efdcbd2b84210a65b767
Jon Gjengset <jon@thesquareplanet.com>
2025-10-26 22:57:44 +0100

    fix for deprecation in leap

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -316,7 +316,8 @@ require("lazy").setup({
 	{
 		'ggandor/leap.nvim',
 		config = function()
-			require('leap').create_default_mappings()
+			vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+			vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
 		end
 	},
 	-- better %
