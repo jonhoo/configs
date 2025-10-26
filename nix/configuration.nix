890a245a8b7c202bd00552563d2557c46b5b937b
Jon Gjengset <jon@thesquareplanet.com>
2025-10-26 21:24:49 +0100

    fewer boot options

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -24,6 +24,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 7;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
